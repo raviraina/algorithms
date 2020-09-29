6edb19f698181ffa78dcc796774e68ee76b05c64
raviraina <ravi.raina@brentwood.ca>
2020-09-29 16:22:31 -0400

    added sorts

--- src/sortAlgorithms/InsertionSort.java
+++ src/sortAlgorithms/InsertionSort.java
@@ -5,7 +5,7 @@ public class InsertionSort {
 	public static void insertionSort(int[] arr) {
 
 		int n = arr.length;
-		
+
 		for (int i = 1; i < n; ++i) {
 			int p = arr[i];
 			int j = i - 1;
@@ -14,7 +14,7 @@ public class InsertionSort {
 				arr[j + 1] = arr[j];
 				j = j - 1;
 			}
-			
+
 			arr[j + 1] = p;
 		}
 	}
--- src/sortAlgorithms/SelectionSort.java
+++ src/sortAlgorithms/SelectionSort.java
@@ -1,5 +1,8 @@
 package sortAlgorithms;
 
 public class SelectionSort {
-
+	
+	public static void selectionSort() {
+		
+	}
 }
